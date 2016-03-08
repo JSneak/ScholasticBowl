f3cb7ff654d4f7247ab152cfb2583fb23058c94a
JSneak <kim2087@students.d211.org>
2016-03-08 11:20:02 -0600

    Files

---
+++ Old Stuff/index.js
@@ -0,0 +1,97 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var express = require('express'); //Module for interface
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var bodyParser = require('body-parser');
+//app.use(bodyParser.json({limit: '50mb'}));
+//app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json({ type: 'application/*+json' })); 
+var HostClass = require('./HostClass');
+var UserClass = require('./UserClass');
+var HostSession = [];//This contains the DJ Name and Code 
+var UserSession = [];//This contains the Guest Name, the code entered, and their vote 
+
+//socket.broadcast.emit sends data back to the caller 
+
+io.on('connection', function (socket) {
+  var addedUser = false;
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', function (data) {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', function (username) {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+  });
+  
+  socket.on('check code',function(code)
+  {
+	  if(username = HostSession.HostCode)//Get the proper Host Code with the code
+	  socket.broadcast.emit('code Result', {
+		  result: result
+	  }); 
+  )};
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', function () {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+    }
+  });
+});
+
+
+function send404Response(response){
+	response.writeHead(404, {"Content-Type": "text/plain"});
+	response.write("Error 404: Page not found!");
+	response.end();
+};
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(8080, function () {
+  console.log('Server listening at port %d 8080');
+});
